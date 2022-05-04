a3c7fd33f55cd805966bb75af89446a20a465f83
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-05-04 17:37:21 +0600

    created firebase init and config

---
+++ src/firebase.init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4sxy04XR_3kcD4oqCSf8erVdSIJHw90k",
+    authDomain: "warehouse-techsolution.firebaseapp.com",
+    projectId: "warehouse-techsolution",
+    storageBucket: "warehouse-techsolution.appspot.com",
+    messagingSenderId: "709703709517",
+    appId: "1:709703709517:web:d7d44145c350f6377cfd04"
+  };
+
+  const app = initializeApp(firebaseConfig);
